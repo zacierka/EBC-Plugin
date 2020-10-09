7b118cfe42b455f912fbf520303b9100042d313c
Switch <mczacierka@gmail.com>
2020-10-08 20:33:46 -0400

    RabbitMQ Basic Impl

--- src/main/java/com/eboyclique/switchmod/Switchmod.java
+++ src/main/java/com/eboyclique/switchmod/Switchmod.java
@@ -1,15 +1,31 @@
+///////////////////////////////////
+// Main class of Switchmod Plugin. This class instantiates most needed variables.
+// CLASS: Switchmod
+// VERSION: 1.0.0
+//
+//  PROGRAMMER: Switch
+//  OCT 8       Initial Impl
+//
+///////////////////////////////////
 package com.eboyclique.switchmod;
 
+import com.eboyclique.switchmod.rabbit.Producer;
+import com.eboyclique.switchmod.rabbit.QueueConsumer;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
 
 /**
  * Driver for switchmod plugin. This plugin interfaces with JMS to communicate with EBC Discord Bot (JDA).
  */
 @SuppressWarnings("unused")
 public final class Switchmod extends JavaPlugin {
-
+    public QueueConsumer consumer = null;
+    public Thread consumerThread = null;
+    public Producer producer = null;
     public Switchmod() {
         super();
+        createMQ();
     }
 
     /**
@@ -32,5 +48,17 @@ public final class Switchmod extends JavaPlugin {
         System.out.println("***********************************\n");
         System.out.println("Halting Switchmod\n");
         System.out.println("***********************************");
+    }
+
+    private void createMQ() {
+        try {
+            consumer = new QueueConsumer("mc");
+            consumerThread = new Thread(consumer);
+            consumerThread.start();
+
+            producer = new Producer("discord");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
